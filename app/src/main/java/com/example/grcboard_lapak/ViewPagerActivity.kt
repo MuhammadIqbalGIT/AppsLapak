b9c8aef32bb4259ddd18c711e52f24f85dc82ff7
iqbal <iqbalvzr@gmail.com>
2022-09-26 07:25:21 +0700

    View Pager + Splash Screen

---
+++ app/src/main/java/com/example/grcboard_lapak/ViewPagerActivity.kt
@@ -0,0 +1,24 @@
+package com.example.grcboard_lapak
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
+
+class ViewPagerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_pager)
+
+
+        val viewPager : ViewPager2 = findViewById(R.id.view_PagerOnBoarding)
+
+        val fragmentViewPager :  ArrayList<Fragment> = arrayListOf(
+            PageOneOnBoardingFragment(),
+            PageTwoOnBoardingFragment(),
+            PagetThreeOnBoardingFragment()
+        )
+
+        val adapterViewPager : ViewPagerAdapter (fr)
+    }
+}
